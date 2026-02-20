0877ab26fdb8a3df2a83bb9e8194532633997689
shwncho <shwncho4@gmail.com>
2026-02-20 10:20:16 +0900

    refactor: auto-offset-reset earliest -> latest 변경

--- src/main/java/com/stock/stockserver/infrastructure/config/KafkaConfig.java
+++ src/main/java/com/stock/stockserver/infrastructure/config/KafkaConfig.java
@@ -60,7 +60,7 @@ public class KafkaConfig {
         props.put(ConsumerConfig.GROUP_ID_CONFIG, groupId);
         props.put(ConsumerConfig.KEY_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
         props.put(ConsumerConfig.VALUE_DESERIALIZER_CLASS_CONFIG, StringDeserializer.class);
-        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "earliest");
+        props.put(ConsumerConfig.AUTO_OFFSET_RESET_CONFIG, "latest");
         props.put(ConsumerConfig.ENABLE_AUTO_COMMIT_CONFIG, false);
         return new DefaultKafkaConsumerFactory<>(props);
     }
